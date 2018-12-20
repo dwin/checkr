9534819a955b3872a227fc0aa6d77020725ebb21
Darwin Smith II <172265+dwin@users.noreply.github.com>
2018-12-20 13:49:29 -0500

    Add Test Report ID

--- report_test.go
+++ report_test.go
@@ -1,9 +1,9 @@
 package checkr
 
 import (
-	"fmt"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -11,8 +11,11 @@ func TestReport(t *testing.T) {
 	// Load Key & Create Client
 	apiKey := LoadKey(t)
 	c := NewClient(apiKey)
-
-	report, err := c.RetrieveReport("")
+	// Get Candidate for Report Retrieval
+	cand, err := c.RetrieveCandidate("aa192591adb0bfd08ae1969d")
 	require.NoError(t, err)
-	fmt.Println(report)
+
+	report, err := c.RetrieveReport(cand.ReportIDs[0])
+	require.NoError(t, err)
+	assert.NotEmpty(t, report.ID)
 }
