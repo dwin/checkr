f64928334e7e8e853c2f934831e8ebbfbd2cda57
Darwin Smith II <dwin@dlsmi.com>
2019-01-24 15:02:57 -0500

    Add Invitations

--- invitation.go
+++ invitation.go
@@ -1,8 +1,14 @@
 package checkr
 
-import "time"
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
 
 // Invitation ...
+// Represents a background check invitation. The candidate will receive an email to submit their information.
+// https://docs.checkr.com/#invitation
 type Invitation struct {
 	ID            string     `json:"id"`
 	Status        string     `json:"status"`
@@ -15,4 +21,86 @@ type Invitation struct {
 	Object        string     `json:"object"`
 	CreatedAt     time.Time  `json:"created_at"`
 	CandidateID   string     `json:"candidate_id"`
+}
+
+// CreateInvitation is used to create a new Invitation.
+func (c *Client) CreateInvitation(pkg, candidateID string) (*Invitation, error) {
+	invite := Invitation{
+		Package:     pkg,
+		CandidateID: candidateID,
+	}
+	// Handle Request
+	resp, err := c.R().SetBody(invite).SetResult(&Invitation{}).SetError(&ErrorResponse{}).Post("/invitations")
+	if err != nil {
+		return nil, err
+	}
+	// Check for expected response
+	if resp.StatusCode() != http.StatusCreated {
+		errResp := resp.Error().(*ErrorResponse)
+		err = fmt.Errorf("Checkr Error: %s", errResp.Error)
+		return nil, err
+	}
+
+	return resp.Result().(*Invitation), nil
+}
+
+// CancelInvitation ...
+func (c *Client) CancelInvitation(invitationID string) (*Invitation, error) {
+	// Handle Request
+	resp, err := c.R().SetResult(&Invitation{}).SetError(&ErrorResponse{}).Delete("/invitations/" + invitationID)
+	if err != nil {
+		return nil, err
+	}
+	// Check for expected response
+	if resp.StatusCode() != http.StatusOK {
+		errResp := resp.Error().(*ErrorResponse)
+		err = fmt.Errorf("Checkr Error: %s", errResp.Error)
+		return nil, err
+	}
+
+	return resp.Result().(*Invitation), nil
+}
+
+// RetrieveInvitation ...
+func (c *Client) RetrieveInvitation(invitationID string) (*Invitation, error) {
+	// Handle Request
+	resp, err := c.R().SetResult(&Invitation{}).SetError(&ErrorResponse{}).Get("/invitations/" + invitationID)
+	if err != nil {
+		return nil, err
+	}
+	// Check for expected response
+	if resp.StatusCode() != http.StatusOK {
+		errResp := resp.Error().(*ErrorResponse)
+		err = fmt.Errorf("Checkr Error: %s", errResp.Error)
+		return nil, err
+	}
+
+	return resp.Result().(*Invitation), nil
+}
+
+// RetrieveInvitations ...
+// status		string 	values: 'pending', 'completed', 'expired'
+// candidate_id	string
+// ex. params := map[string]string{
+// 	"status":"pending",
+// }
+// inv, err := c.RetrieveInvitations(params)
+func (c *Client) RetrieveInvitations(params ...map[string]string) (*Invitation, error) {
+	// Handle Request
+	req := c.R().SetResult(&Invitation{}).SetError(&ErrorResponse{})
+	if len(params) > 1 {
+		req.SetQueryParams(params[0])
+	}
+	resp, err := req.Get("/invitations")
+	if err != nil {
+		return nil, err
+	}
+	// Check for expected response
+	if resp.StatusCode() != http.StatusOK {
+		errResp := resp.Error().(*ErrorResponse)
+		err = fmt.Errorf("Checkr Error: %s", errResp.Error)
+		return nil, err
+	}
+
+	return resp.Result().(*Invitation), nil
 }
---
+++ invitation_test.go
@@ -0,0 +1,36 @@
+package checkr
+
+import (
+	"testing"
+
+	randomdata "github.com/Pallinder/go-randomdata"
+	"github.com/stretchr/testify/require"
+)
+
+func TestInvitation(t *testing.T) {
+	// Load Key & Create Client
+	apiKey := LoadKey(t)
+	c := NewClient(apiKey)
+
+	// Create Candidate for Testing
+	candidate := Candidate{
+		FirstName:           randomdata.FirstName(randomdata.RandomGender),
+		NoMiddleName:        true,
+		LastName:            randomdata.LastName(),
+		Email:               randomdata.Email(),
+		DOB:                 "1970-01-25",
+		SSN:                 "111-11-2001",
+		Zipcode:             "49503",
+		DriverLicenseNumber: "F1112001",
+		DriverLicenseState:  "CA",
+		CustomID:            randomdata.Digits(14),
+	}
+	cand, err := c.CreateCandidate(candidate)
+	require.NoError(t, err)
+	require.NotNil(t, cand)
+
+	// Create Invitation
+	inv, err := c.CreateInvitation("driver_pro", cand.ID)
+	require.NoError(t, err)
+	require.NotNil(t, inv)
+}
